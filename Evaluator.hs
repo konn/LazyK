7336e24175d2625fc739fd42b09be130f4bcc7de
Hiromi Ishii <konn.jinro_at_gmail.com>
2011-04-28 00:53:38 +0900

    changed to use stupid way to output

--- Evaluator.hs
+++ Evaluator.hs
@@ -7,6 +7,7 @@ import Utils
 import Control.Applicative
 import Text.Parsec
 import System.Environment
+import System.IO
 
 runLazyK :: String -> IO ()
 runLazyK src = do
@@ -15,10 +16,10 @@ runLazyK src = do
   case ans of
     Left err  -> print err
     Right ans -> do
-      let mout = fromExpr $ eval $ App ans input
+      let !mout = fromExpr $ App ans input
       case mout of
         Nothing -> putStrLn "ERR: Nothing"
-        Just a  -> putStr a
+        Just a  -> mapM_ (\c -> putChar c >> hFlush stdout) a
 
 main = do
   args <- getArgs
